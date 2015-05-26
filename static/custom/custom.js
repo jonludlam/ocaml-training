e528195d78e7e8794fc1104ee3eee73650e0ac81
Euan Harris <euan.harris@citrix.com>
2015-05-26 14:45:14 +0100

    IOcaml: Disable notebook autosave
    From this Stack Overflow answer:
       http://stackoverflow.com/questions/25631344/turn-off-autosave-in-ipython-notebook
    
    Signed-off-by: Euan Harris <euan.harris@citrix.com>

--- static/custom/custom.js
+++ static/custom/custom.js
@@ -227,3 +227,6 @@ $([IPython.events]).on('shell_reply.Kernel', function() {
 });
 */
 
+$([IPython.events]).on("notebook_loaded.Notebook", function () {
+  IPython.notebook.set_autosave_interval(0);
+});
